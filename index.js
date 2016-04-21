1be9ae048cf05e8484fd860882b0bbfb4d6d51ac
Romain Durand <sirius2006@gmail.com>
2016-04-22 01:03:30 +0200

    added route + switch logic

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var _ = require("lodash");
 var client = Wemo.Search();
 var devices = [];
 
-client.on('found', function(device) {
+client.on('found', function (device) {
     console.log("Device found : " + device.friendlyName);
-    var isDeviceRegistered = !!devices.find(function(registeredDevice) {
+    var isDeviceRegistered = !!devices.find(function (registeredDevice) {
         return registeredDevice.name === device.friendlyName;
     });
-    
-    if(isDeviceRegistered) {
+
+    if (isDeviceRegistered) {
         console.log("Device '" + device.friendlyName + "' already registered");
         return;
     }
-    
+
     devices.push({
         name: device.friendlyName,
         ip: device.ip,
@@ -23,19 +23,61 @@ client.on('found', function(device) {
     });
     console.log("Device registered.");
 });
-app.get('/', function(req, res) {
+
+app.get('/switch/:name', function (req, res) {
+    var wemo;
+    if (!!getDevice(req.params.name)) {
+        Wemo.Search(req.params.name, function (err, device) {
+
+        });
+    }
+    toggleDevice({
+        name: req.params.name,
+        onSuccess: function () {
+            res.send({
+                success: true
+            });
+        },
+        onError: function (err) {
+            res.send({
+                success: false,
+                message: err
+            });
+        }
+    });
+});
+
+function toggleDevice(conf) {
+    var device = getDevice(conf.name);
+    var wemo = new Wemo(device.ip, device.port);
+    wemo.getBinaryState(function (err, result) {
+        if (err) {
+            conf.onError(err);
+            return;
+        }
+        wemo.setBinaryState(-result + 1, function (err, result) {
+            if (err) {
+                conf.onError(err);
+                return;
+            }
+            conf.onSuccess();
+        });
+    });
+}
+
+app.get('/', function (req, res) {
     res.send('Hello World!');
     var wemo = new Wemo(devices[0].ip, devices[0].port);
-    wemo.getBinaryState(function(err, result) {
+    wemo.getBinaryState(function (err, result) {
         if (err) console.error(err);
-        wemo.setBinaryState(-result + 1, function(err, result) { // switch on
+        wemo.setBinaryState(-result + 1, function (err, result) { // switch on
             if (err) console.error(err);
             console.log("light switch");
         });
     });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
